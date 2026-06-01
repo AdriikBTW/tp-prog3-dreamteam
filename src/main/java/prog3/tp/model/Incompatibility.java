9dea45af4fe38f64c31cb655d377cc639751fb49
Nahuel <nahuelnano06@gmail.com>
2026-06-01 10:53:01 -0300

    methods for the incompatibility class

--- src/main/java/prog3/tp/model/Incompatibility.java
+++ src/main/java/prog3/tp/model/Incompatibility.java
@@ -1,5 +1,25 @@
 package prog3.tp.model;
 
-public class Incompatibility {
+import java.util.List;
+import java.util.ArrayList;
 
+public class Incompatibility {
+	
+	private Employee _employee1;
+	private Employee _employee2;
+	
+	public Incompatibility(Employee E1,Employee E2) {
+		_employee1 = E1;
+		_employee2 = E2;
+	}
+	
+	public List<Employee> getIncompatibility() {
+		
+		 List <Employee>  ret = new ArrayList<>();
+		 
+		 ret.add(_employee1);  
+		 ret.add(_employee2);
+		 
+		 return ret;
+	}
 }
